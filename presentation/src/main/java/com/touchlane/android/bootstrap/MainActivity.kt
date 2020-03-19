c8d69a37195f536c43e6fcb1fd0edb9bd2d0b54b
Pavel Shurmilov <pckeycalculator@gmail.com>
2020-03-19 18:14:31 +0300

    Test pr

--- presentation/src/main/java/com/touchlane/android/bootstrap/MainActivity.kt
+++ presentation/src/main/java/com/touchlane/android/bootstrap/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.touchlane.android.bootstrap
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "TEST", Toast.LENGTH_SHORT).show()
     }
 }
